e3e082f0b95cdceafc1ae1f6e33068c4861857bc
Megan <meggrabill5@gmail.com>
2015-01-02 15:19:36 -0500

    getting express js and node working

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express')
+var app = express()
+app.use(express.static(__dirname + '/assets'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/about', function(req, res) {
+  res.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/contact', function(req, res) {
+  res.sendFile(__dirname + '/views/contact.html');
+});
+
+app.get('/portfolio', function(req, res) {
+  res.sendFile(__dirname + '/views/portfolio.html');
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+})
